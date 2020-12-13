638a12131e9ae14dc72030f9da366c727ad9646d
Mariana Mattana <mariana.mattana83@gmail.com>
2020-12-13 11:42:12 -0300

    Starting calendar

--- src/components/heatmap/heatmap.jsx
+++ src/components/heatmap/heatmap.jsx
@@ -1,9 +1,21 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { isEmpty } from 'ramda'
+import moment from 'moment';
 import { fetchDates } from '../../store/actions/dates'
+import { Grid, Cell } from 'styled-css-grid'
 import { Button } from '../styled/Lib'
 
+const DAYS = (yy) => {
+  const days = []
+  let dateStart = moment(`01/01/${yy}`, "DD/MM/YYYY")
+  let dateEnd = moment(`31/12/${yy}`, "DD/MM/YYYY")
+  while ( dateStart <= dateEnd ){
+    days.push(dateStart.toDate());
+    dateStart = dateStart.clone().add(1, 'd')
+  }
+  return days
+ }
 class Heatmap extends Component {
 
   constructor(props) {
@@ -23,18 +35,20 @@ class Heatmap extends Component {
 
   render(){
     const { dateList } = this.props
+    const days = DAYS(2016)
     debugger
     return(
       <Fragment>
-        <Button submit/>
-        <div>
-          {!isEmpty(dateList) && (
-              <ul>
-                {Object.values(dateList).map(key => <li ><Button submit> {Object.keys(key)} </Button>
-                </li>)}
-            </ul>
-          )}
-        </div>
+        {!isEmpty(days) && (
+          <Grid flow="column" rows={7} columns={53} gap="2px">
+            {Object.values(days).map(key => <Cell center><Button submit /></Cell>)}
+          </Grid>
+        )}
+        {!isEmpty(dateList) && (
+          <Grid flow="column" rows={7} columns={53} gap="2px">
+            {Object.values(dateList).map(key => <Cell center><Button cancel></Button></Cell>)}
+          </Grid>
+        )}
       </Fragment>
     ) 
   }
